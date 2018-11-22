82a2677c27dac3fe1f53c48d2b547a208b0e86aa
MikeO <10thmountain@zoho.com>
2018-11-22 11:02:23 +0800

    Update tipbot.sql

--- tipbot.sql
+++ tipbot.sql
@@ -59,21 +59,6 @@ CREATE TABLE `tipbot_deposits` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- ----------------------------
---  Table structure for `tipbot_mikeswork`
--- ----------------------------
-DROP TABLE IF EXISTS `tipbot_mikeswork`;
-CREATE TABLE `tipbot_mikeswork` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `task` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `priority` int(11) DEFAULT NULL,
-  `completed` tinyint(1) DEFAULT NULL,
-  `targetdate` date DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ----------------------------
 --  Table structure for `tipbot_tips`
 -- ----------------------------
 DROP TABLE IF EXISTS `tipbot_tips`;
